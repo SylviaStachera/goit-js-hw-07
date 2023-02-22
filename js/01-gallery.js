ae3828559a880f4c83486c112b049d9b44566972
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-22 19:16:47 +0100

    Delegation part 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,21 +8,39 @@ const gallery = document.querySelector('.gallery')
 
 //Tworzenie div item
 
-galleryItems.forEach((n) => {
-//console.log(n.preview)
-const galleryItem = `<div class="gallery__item">
-<a class="gallery__link" href="${n.preview}">
+galleryItems.forEach(n => {
+	//console.log(n.preview)
+	const galleryItem = `<div class="gallery__item">
+<a class="gallery__link" href="${n.original}">
   <img
     class="gallery__image"
-    src="${n.original}"
-    data-source="${n.preview}"
+    src="${n.preview}"
+    data-source="${n.original}"
     alt="${n.description}"
   />
 </a>
-</div>`  
+</div>`
 
-gallery.insertAdjacentHTML('afterbegin', galleryItem)
+	gallery.insertAdjacentHTML('afterbegin', galleryItem)
 })
+//=======================================================
+//Tworzenie delegowania zdarzeń
 
+const selectImg = e => {
+	e.preventDefault()
+	if (e.target.nodeName !== 'IMG') {
+		return
+	}
+	const selectLargeImg = e.target.dataset.source
 
+	//console.log(selectLargeImg)
 
+	const galleryLargeItem = document.querySelector('.gallery__image')
+	// console.log(galleryLargeItem)
+	// console.log(galleryLargeItem.dataset.source)
+
+	galleryLargeItem.setAttribute('src', galleryLargeItem.dataset.source)
+}
+
+//Implementacja delegowania
+gallery.addEventListener('click', selectImg)
