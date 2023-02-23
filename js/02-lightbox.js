97b22c8a0205eb3bf5025de175084bbc7e2be167
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-02-23 18:27:26 +0100

    Update task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+
+//===================================================
+const gallery = document.querySelector('.gallery')
+
+//Tworzenie div item
+galleryItems.forEach(n => {
+	const galleryItem = `<a class="gallery__item" href="${n.original}">
+  <img
+    class="gallery__image"
+    src="${n.preview}"
+    alt="${n.description}"
+    title="${n.description}"
+  />
+</a>`
+
+	gallery.insertAdjacentHTML('afterbegin', galleryItem)
+})
+//=======================================================
+// 	//Użycie biblioteki simplelightbox do wyświetlania img na pełnym ekranie i przewijania
+
+var lightbox = new SimpleLightbox('.gallery a', {
+	captionsData: 'title',
+	captionDelay: 250,
+})
